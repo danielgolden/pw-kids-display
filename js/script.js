1747daafd01bc89bff83b47403ac6f936971e14f
danielgolden <danielgolden90@gmail.com>
2014-04-25 15:14:51 -0400

    Add's restyled header and perspective animation

--- js/script.js
+++ js/script.js
@@ -22,4 +22,38 @@ $(function() {
 		$('.container').removeClass('active');
 		$('.child_profile').hide().removeClass('bounceOutDown animated');
 	});
+
+	$(document).ready(function() {
+	    var scrollorama = $.scrollorama({
+	        blocks:'.wrapper'
+	    });
+
+	    scrollorama.animate('.container',{
+	        duration: 1000,
+	        property:'opacity',
+	        start: 0,
+	        finish: 1
+	    })
+	});
+
+	var scrollPosition
+	var rotatePosition
+
+	$(window).scroll(function() {
+		scrollPosition = $(this).scrollTop();
+		rotatePosition = - scrollPosition / 4 + 65;
+		scale = scrollPosition / 700 + .625;
+		footerOpacity = scrollPosition / 400;
+
+		if(rotatePosition < 1) {
+			rotatePosition = 0;
+		} if (scale > 1) {
+			scale = 1;
+		} else if (scale < .625) {
+			scale = .625;
+		}
+
+		$('.container').attr('style', '-webkit-transform: rotateX('+ rotatePosition +'deg) scale('+ scale +', '+ scale +');');
+		$('footer, .cta').attr('style', 'opacity:'+ footerOpacity +';');
+	});
 });
