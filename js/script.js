4face0273cc42429d7bb912389d2b7cfe6a3ae13
danielgolden <danielgolden90@gmail.com>
2014-04-25 15:21:08 -0400

    Adjusts timing of timeline animation

--- js/script.js
+++ js/script.js
@@ -23,26 +23,13 @@ $(function() {
 		$('.child_profile').hide().removeClass('bounceOutDown animated');
 	});
 
-	$(document).ready(function() {
-	    var scrollorama = $.scrollorama({
-	        blocks:'.wrapper'
-	    });
-
-	    scrollorama.animate('.container',{
-	        duration: 1000,
-	        property:'opacity',
-	        start: 0,
-	        finish: 1
-	    })
-	});
-
 	var scrollPosition
 	var rotatePosition
 
 	$(window).scroll(function() {
 		scrollPosition = $(this).scrollTop();
-		rotatePosition = - scrollPosition / 4 + 65;
-		scale = scrollPosition / 700 + .625;
+		rotatePosition = - scrollPosition / 6.3 + 65;
+		scale = scrollPosition / 1080 + .625;
 		footerOpacity = scrollPosition / 400;
 
 		if(rotatePosition < 1) {
